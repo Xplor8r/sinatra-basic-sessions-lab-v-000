60280f56712ce58645a61f6a427a48b44b5a923a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-10 19:19:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :session
+    set :session_secret, "super dooper top secret"
+  end
   get '/' do
     erb :index
   end
+
+  post '/checkout'
+  
 end
